1f8526379e25170d6ac04b22215e6fa4043f2f46
2hyeong <tubs5840@naver.com>
2023-11-15 05:37:27 +0900

    moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [level, setLevel] = useState(1);
+  let [score, setScore] = useState(1);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <div className='game-title'>
+        래빗러쉬
+      </div>
+      <div className='record'>
+        <h3> 레벨: { level }  &#40;최고기록: { score }&#41;</h3>
+        <hr/>
+      </div>
+    </>
   );
 }
 
